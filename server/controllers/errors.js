c25bedf85b8c385321465e62d3ffcc9ae1cb61a9
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2019-10-28 20:22:01 +0700

    Update errors.js

--- server/controllers/errors.js
+++ server/controllers/errors.js
@@ -1,1 +1,4 @@
-exports.get404 = (req, res) => res.status(404).render("404");
+exports.get404 = (req, res) => res.status(404).render("404", {
+  pageTitle: "Travel Aloha - Page not found",
+  user: req.user
+});
