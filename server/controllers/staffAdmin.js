85f99279252b80f2f5bc280a2eba6c87a5ce357a
einsxort <einsxort127@gmail.com>
2019-11-05 12:31:43 +0700

    [ADD] Working with login system

--- server/controllers/staffAdmin.js
+++ server/controllers/staffAdmin.js
@@ -2,17 +2,14 @@ const connector = require("../db/db")
 
 exports.getApplicationForm = async function(request, response) {
     try {
-        var result = await connector.query(`SELECT * FROM user`)
+        var result = await connector.query(`SELECT * FROM user WHERE username='${request.user.username}'`)
         response.render('staff_admin/recruiting', {
             pageTitle: 'TravelAloha - StaffRecruiting',
             user: request.user,
             data: JSON.stringify(result[0])
         })
     } catch (error) {
-        response.send(`
-        <!DOCTYPE html><head><title></title></head>
-        <body><h1>Something was wrong</h1></body>
-        `)
+        response.redirect('/login')
     }
 }
 
