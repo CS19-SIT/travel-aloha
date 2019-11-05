d1c7e780140dd262d6f8e76d4c8d2fedc002cca3
Kiatisak <earth4971@gmail.com>
2019-11-05 18:58:29 +0700

    [ADD]  More detail in model

--- server/models/hotelBookingModel/hotel.js
+++ server/models/hotelBookingModel/hotel.js
@@ -1,7 +1,7 @@
 const db = require("../../db/db"); //connect to db
  // we also need roomid
 
-exports.getHotelRoom = async (hotelId,roomId) => {
+exports.getHotelRoomOne = async (hotelId,roomId) => {
     try {
       const result = await db.query("SELECT * FROM room WHERE hotelId_room = ? and roomId = ?", [
         hotelId,
@@ -14,6 +14,18 @@ exports.getHotelRoom = async (hotelId,roomId) => {
       return result[0][0];
     } catch (err) {
       throw new Error(`[ERR] getHotelRoomID: ${err}`);
+    }
+  };
+  
+  exports.getAllRoomInHotel = async (hotelId)=>{
+    try {
+      const resultAll = await db.query("select * from hotel,room_head where hotelIdroom = ?",[hotelId]);
+      if (result[0].length < 1) {
+        throw new Error(`Cannot find your room ${hotelId}.`);
+      } 
+      return resultAll[0][0];
+    } catch (err) {
+      throw new Error(`[ERR] getHotelALlRoomID: ${err}`);
     }
   };
  
