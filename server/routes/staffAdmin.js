f02d93fcb926f78bbe49a952d032812ffc85fc20
Napat Jamjan <napat.czs@mail.kmutt.ac.th>
2019-11-05 12:25:58 +0700

    Delete Test stuff

--- server/routes/staffAdmin.js
+++ server/routes/staffAdmin.js
@@ -7,20 +7,6 @@ router.get('/recruiting', staffAdminController.getApplicationForm)
 router.get('/requisition', staffAdminController.getStaffCandidatesList)
 router.get('/manager', staffAdminController.getAllExistedStaffDetail)
 
-router.get('/hahayes', (req, res) => {
-    res.send('Hello');
-});
-
-router.get('/test/:userId', (req, res) =>{
-    const id = req.params.userId;
-    if(id < 5){
-        res.send(`HAHAYES ${id}`);
-    }
-    else{
-        res.send(`NO ${id}`);
-    }
-});
-
 //////////EXPERIMENTAL//////////
 router.get('/recruiting/:userId', staffAdminController.getApplicationForm2);
 
