e9e06cac3fb438885e3af4c9ec96597d08842d2e
domesama <kumasamath@gmail.com>
2019-11-05 12:26:07 +0700

    [Fix]You had a typo

--- server/routes/hotel-booking.js
+++ server/routes/hotel-booking.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const hotelBookingController = require("../controllers/hotel-booking");
+const hotelBookingController = require("../controllers/hotelBookingController/hotel-booking");
 
 router.get("/hotel-booking", hotelBookingController.getHotelBooking);
 
