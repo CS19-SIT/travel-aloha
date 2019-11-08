3636ef80e460a7f0ea1e7397252f32302b9e1c38
Krerkthad <kerkthud_1@hotmail.com>
2019-11-08 17:28:38 +0700

    [UPDATE] controller, model, route of Rating_Review

---
+++ server/models/Rating_Review.js
@@ -0,0 +1,26 @@
+const db = require("../db/db");
+
+exports.getAllHotelReview = async() =>{
+    try{
+        const result = await db.query(`SELECT * FROM hotel`);
+        
+        return result[0];
+    } catch (err) {
+        throw new Error(`[ERR] getAllHotel: ${err}`);
+    }
+};
+
+exports.modelUpdateHotel = async(data)=>{
+    try {
+        console.log("From mode",data);
+        await db.query("UPDATE hotel set hotelAddress = ? , hotelTelNumber = ?, hotelEmail = ? WHERE hotelId = ?",[
+            data.hotelAddress,
+            data.hotelTelNumber,
+            data.hotelEmail,
+            data.hotelId
+        ]);
+    } catch (err) {
+        throw new Error(`[ERR] modelUpdateHotel: ${err}`);
+    }
+}
+
