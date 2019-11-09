7bc9620985066a6ebb641b92f8aa6fe639df4541
flukerbooker <win.vorapol52@gmail.com>
2019-11-09 19:37:16 +0700

    [UPDATE] All pages tittle

--- server/routes/Contact_System.js
+++ server/routes/Contact_System.js
@@ -1,52 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-  /* GET home page. */
-  router.get('/', function(req, res, next) {
-    res.render('Contact_System/landingPage', {
-      pageTitle: 'TravelAloha-sdasdas',
-      user: req.user,
-      page:'LandingPage', 
-      menuId:'landingPage'});
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('Contact_System/landingPage', {
+    pageTitle: 'TravelAloha-ContactSystem',
+    user: req.user
   });
-
-router.get('/hotelDetail', function(req, res, next) {
-  res.render('Contact_System/hotelDetail', {
-    pageTitle: 'TravelAloha-sdasdas',
-    user: req.user,
-    page:'Hotel Detail', 
-    menuId:'hotelDetail'
+});
+/* GET home page. */
+router.get('/informationFlow', function (req, res, next) {
+  res.render('Contact_System/informationFlow', {
+    pageTitle: 'TravelAloha-Information',
+    user: req.user
   });
 });
 
 /* GET home page. */
-router.get('/informationFlow', function(req, res, next) {
-    res.render('Contact_System/informationFlow', {
-      pageTitle: 'TravelAloha-sdasdas',
-      user: req.user,
-      page:'Information', 
-      menuId:'informationFlow'
-    });
+router.get('/newAirlineInfo', function (req, res, next) {
+  res.render('Contact_System/newAirlineInfo', {
+    pageTitle: 'TravelAloha-NewAirline',
+    user: req.user
   });
+});
 
 /* GET home page. */
-router.get('/newAirlineInfo', function(req, res, next) {
-    res.render('Contact_System/newAirlineInfo', {
-      pageTitle: 'TravelAloha-sdasdas',
-      user: req.user,
-      page:'New Airline',  
-      menuId:'newAirlineInfo'
-    });
+router.get('/newHotelInfo', function (req, res, next) {
+  res.render('Contact_System/newHotelInfo', {
+    pageTitle: 'TravelAloha-NewHotel',
+    user: req.user
   });
+});
 
-  /* GET home page. */
-router.get('/newHotelInfo', function(req, res, next) {
-    res.render('Contact_System/newHotelInfo', {
-      pageTitle: 'TravelAloha-sdasdas',
-      user: req.user,
-      page:'New Hotel', 
-      menuId:'newHotelInfo'
-    });
+router.get('/hotelDetail', function (req, res, next) {
+  res.render('Contact_System/hotelDetail', {
+    pageTitle: 'TravelAloha-Detail',
+    user: req.user
   });
+});
 
-  module.exports = router;
+module.exports = router;
