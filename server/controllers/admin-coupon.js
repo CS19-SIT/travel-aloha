42fae5343f5511f4686d0d975ec8f17eb72f89b6
CHARUPHAT CHINDAWORRASATE <charuphat.chindaworrasate@mail.kmutt.ac.th>
2019-11-11 20:11:32 +0700

    [ADD] additional routes and update controll for  coupon

--- server/controllers/admin-coupon.js
+++ server/controllers/admin-coupon.js
@@ -1,4 +1,60 @@
-exports.getIndex = (req, res) => res.render("admin/admin-coupon", {
-    pageTitle: "Travel Aloha - Admin - Coupon Management",
-    user: req.user
-});
+const Coupon = require("../models/coupon");
+
+exports.getIndex = async (req, res) => {
+    try {
+        //  let data = await Hotel.getAllHotel();
+
+        res.render("admin/admin-coupon", {
+            pageTitle: "Travel Aloha - Admin - Coupon Management",
+            user: req.user
+            //   hotel: data
+        });
+    } catch (err) {
+        res.sendStatus(404);
+    }
+};
+
+exports.createCoupon = async (req, res) => {
+    try {
+        await Coupon.createCoupon(req.body);
+        res.sendStatus(204);
+    } catch (err) {
+        res.sendStatus(404);
+    }
+};
+
+exports.editCoupon = async (req, res) => {
+    try {
+        await Coupon.editCoupon(req.body);
+        res.sendStatus(204);
+    } catch (err) {
+        res.sendStatus(404);
+    }
+};
+
+exports.deleteCoupon = async (req, res) => {
+    try {
+        await Coupon.deleteCoupon(req.body);
+        res.sendStatus(204);
+    } catch (err) {
+        res.sendStatus(404);
+    }
+};
+
+exports.searchCoupon = async (req, res) => {
+    try {
+        await Coupon.findCoupon(req.body);
+        res.sendStatus(204);
+    } catch (err) {
+        res.sendStatus(404);
+    }
+};
+
+exports.generateCouponCode = async (req, res) => {
+    try {
+        await Coupon.generateCouponCode(req.body);
+        res.sendStatus(204);
+    } catch (err) {
+        res.sendStatus(404);
+    }
+};
--- server/routes/admin-coupon.js
+++ server/routes/admin-coupon.js
@@ -5,5 +5,10 @@ const adminCouponController = require("../controllers/admin-coupon");
 const authMiddleware = require("../middlewares/auth");
 
 router.get("/", authMiddleware.isAuthenticated, adminCouponController.getIndex);
+router.get("/", authMiddleware.isAuthenticated, adminCouponController.createCoupon);
+router.get("/", authMiddleware.isAuthenticated, adminCouponController.editCoupon);
+router.get("/", authMiddleware.isAuthenticated, adminCouponController.deleteCoupon);
+router.get("/", authMiddleware.isAuthenticated, adminCouponController.searchCoupon);
+router.get("/", authMiddleware.isAuthenticated, adminCouponController.generateCouponCode);
 
 module.exports = router;
