06d9dcfb47cb5c8cdfffff162143f8eb0e7789ae
flukerbooker <win.vorapol52@gmail.com>
2019-11-11 22:08:14 +0700

    [UPDATE] Routes fixed

--- server/routes/Contact_System.js
+++ server/routes/Contact_System.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('Contact_System/landingPage', {
     pageTitle: 'TravelAloha-ContactSystem',
     user: req.user
   });
 });
-/* GET home page. */
+
 router.get('/informationFlow', function (req, res, next) {
   res.render('Contact_System/informationFlow', {
     pageTitle: 'TravelAloha-Information',
@@ -16,7 +15,6 @@ router.get('/informationFlow', function (req, res, next) {
   });
 });
 
-/* GET home page. */
 router.get('/newAirlineInfo', function (req, res, next) {
   res.render('Contact_System/newAirlineInfo', {
     pageTitle: 'TravelAloha-NewAirline',
@@ -24,7 +22,6 @@ router.get('/newAirlineInfo', function (req, res, next) {
   });
 });
 
-/* GET home page. */
 router.get('/newHotelInfo', function (req, res, next) {
   res.render('Contact_System/newHotelInfo', {
     pageTitle: 'TravelAloha-NewHotel',
