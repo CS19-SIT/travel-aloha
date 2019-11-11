5896440ca8e0e85a7705d5d685c6fe05a9216650
kritamate.bs@gmail.com <kritamate@windowslive.com>
2019-11-11 18:40:24 +0700

    [ADD]hotel,flightdetailpage

--- server/routes/historySystem.js
+++ server/routes/historySystem.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const authController = require("../controllers/auth");
 const authMiddleware = require("../middlewares/auth");
+router.get('./history/HotelDetail',(req,res)=>{
+    res.render('history/Hotel',{
+        pageTitle: "HotelDetail",
+        user: req.user
+    })
+})
 
 router.get('/history',(req,res) => {
     res.render('history/history',{
@@ -10,6 +16,12 @@ router.get('/history',(req,res) => {
         user: req.user
     })
 })
-
+router.get('./hisrory/flightDetail',(req,res)=>{
+    res.render('history/flight',{
+        pageTitle: "FlightDetail",
+        user: req.user
+    })
+})
 
 module.exports = router;
+
