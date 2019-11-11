23533f053c136f4dd7bfac5b225160d517b7d0d2
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2019-11-12 02:42:50 +0700

    [UPDATE] partial model implementation and rename parameters
    - Renamed findCoupons to searchCoupons
    - findCoupon implementation now independent of searchCoupons
    - Rename parameters to the same name in tables (i.e. from camelCase to snake_case) in function that accept model data (e.g. updateCoupon)

--- server/models/coupon.js
+++ server/models/coupon.js
@@ -1,22 +1,64 @@
 const db = require("../db/db");
 
-exports.findCoupons = async ({
+exports.searchCoupons = async ({
     code,
     name,
     description,
     levels,
-    page,
+    page = 0,
     entriesPerPage = 10
-}) => {
+} = {}) => {
+    try {
+        const result = await db.query(`
+            SELECT DISTINCT coupon.*
+            FROM coupon LEFT JOIN coupon_criteria_level as lvl ON coupon.code = lvl.code
+            WHERE ? OR (
+                (? AND UPPER(coupon.code) LIKE UPPER(?)) OR
+                (? AND UPPER(name) LIKE UPPER(?)) OR
+                (? AND UPPER(description) LIKE UPPER(?))
+            ) AND (lvl.level IN (?) OR ?)
+            LIMIT ?, ?
+        `, [
+            code == null && name == null && description == null,
 
+            code != null,
+            `%${code}%`,
+
+            name != null,
+            `%${name}%`,
+
+            description != null,
+            `%${description}%`,
+
+            levels,
+            levels == null,
+
+            page * entriesPerPage,
+            (page + 1) * entriesPerPage,
+        ]);
+
+        return result[0].map(e => {
+            e.for_every_hotel = !!e.for_every_hotel;
+            e.for_every_airline = !!e.for_every_airline;
+            return e;
+        });
+    } catch (err) {
+        throw new Error(`[ERR] findCoupons: ${err}`)
+    }
 };
 
 exports.findCoupon = async code => {
-    return exports.findCoupons({code: code}).then(coupons => coupons[0]);
-};
+    try {
+        const result = await db.query("SELECT * FROM coupon WHERE code = ?", [code]);
 
-exports.generateCouponCode = async () => {
+        if (result[0].length >= 1) {
+            return result[0];
+        }
 
+        throw new Error(`Coupon code '${code}' not found`);
+    } catch (err) {
+        throw new Error(`[ERR] findCoupon: ${err}`);
+    }
 };
 
 exports.createCoupon = async ({
@@ -26,27 +68,31 @@ exports.createCoupon = async ({
     levels,
     hotels,
     airlines,
-    discountPercentage,
-    startDate,
-    endDate
+    discount_percentage,
+    start_date,
+    end_date
 }) => {
 
 };
 
-exports.editCoupon = async ({
+exports.updateCoupon = async ({
     code,
     name,
     description,
     levels,
     hotels,
     airlines,
-    discountPercentage,
-    startDate,
-    endDate
+    discount_percentage,
+    start_dte,
+    end_date
 }) => {
 
 };
 
 exports.deleteCoupon = async code => {
-
+    try {
+        await db.query("DELETE FROM coupon WHERE code = ?", [code]);
+    } catch (err) {
+        throw new Error(`[ERR] deleteCoupon: ${err}`)
+    }
 };
