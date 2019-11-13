6a3ccae3d91e9b4359dc1f8af7d33e1eecd9f5f6
flukerbooker <win.vorapol52@gmail.com>
2019-11-13 16:59:45 +0700

    [UPDATE] newHotel & newAirline Model

--- server/models/informationFlow.js
+++ server/models/informationFlow.js
@@ -8,42 +8,31 @@ exports.getAllHotel = async () => {
         throw new Error(`[ERR] getAllHotel: ${err}`);
     }
 };
-
-exports.insertNewHotel = async (data) => {
+exports.insertNewHotel = async (hotelId, hotelName, hotelDescription, hotelAddress,
+    hotelTelNumber, hotelContactNumber, hotelEmail, hotelPicture, hotelLogo) => {
     try {
-        // console.log("From mode",data);
-        await db.query("INSERT INTO hotel (hotelId,hotelName,hotelDescription,hotelAddress,hotelTelNumber,hotelContactNumber,hotelEmail,hotelPicture,hotelLogo), VALUES(hotelId = ? ,hotelName = ?,hotelDescription = ?,hotelAddress = ?,hotelTelNumber = ?,hotelContactNumber = ?,hotelEmail = ?,hotelPicture = ?,hotelLogo = ?)", [
-            data.hotelId,
-            data.hotelName,
-            data.hotelDescription,
-            data.hotelAddress,
-            data.hotelTelNumber,
-            data.hotelContactNumber,
-            data.hotelEmail,
-            data.hotelPicture,
-            data.hotelLogo
+        await db.query("Insert into hotel(hotelId,hotelName,hotelDescription,hotelAddress" +
+            "hotelTelNumber,hotelContactNumber,hotelEmail,hotelPicture,hotelLogo) " +
+            "values(`?`,`?`,`?`,`?`,?`,`?`,`?`,`?`,`?`)", [
+            hotelId, hotelName, hotelDescription, hotelAddress, hotelTelNumber,
+            hotelContactNumber, hotelEmail, hotelPicture, hotelLogo
         ]);
-    } catch (err) {
+
+    } catch (error) {
         throw new Error(`[ERR] insertNewHotel: ${err}`);
     }
 }
-
-exports.insertNewAirline = async (data) => {
-    try{
-        await db.query("INSERT INTO new_airline (airline_Id,airlineName,airlineNationality,airlineEmail,airlineDescription,airlineAddress,airlineTelNumber,airlineContactNumber,airlinePicture,airlineLogo), VALUES(airline_Id = ?,airlineName = ?,airlineNationality = ?,airlineEmail = ?,airlineDescription = ?,airlineAddress = ?,airlineTelNumber = ?,airlineContactNumber = ?,airlinePicture = ?,airlineLogo = ?)",
-        [
-            data.airline_Id,
-            data.airlineName,
-            data.airlineNationality,
-            data.airlineEmail,
-            data.airlineDescription,
-            data.airlineAddress,
-            data.airlineTelNumber,
-            data.airlineContactNumber,
-            data.airlinePicture,
-            data.airlineLogo
+exports.insertNewAirline = async (airline_Id, airlineName, airlineNationality, airlineEmail, airlineDescription,
+    airlineAddress, airlineTelNumber, airlineContactNumber, airlinePicture, airlineLogo) => {
+    try {
+        await db.query("Insert into hotel(airline_Id, airlineName, airlineNationality, airlineEmail" +
+            "airlineDescription,airlineAddress, airlineTelNumber, airlineContactNumber, airlinePicture, airlineLogo) " +
+            "values(`?`,`?`,`?`,`?`,?`,`?`,`?`,`?`,`?`,`?`)", [
+            hotelId, hotelName, hotelDescription, hotelAddress, hotelTelNumber,
+            hotelContactNumber, hotelEmail, hotelPicture, hotelLogo
         ]);
-    } catch (err) {
-        throw new Error(`[ERR] insertNewAirline: ${err}`);
+
+    } catch (error) {
+        throw new Error(`[ERR] insertNewHotel: ${err}`);
     }
 }
