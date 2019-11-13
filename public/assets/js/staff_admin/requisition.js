c422a740571b14214a3ccc35b5bdc5a04f9e2124
Napat Jamjan <napat.czs@mail.kmutt.ac.th>
2019-11-13 15:05:59 +0700

    [Alter] BG and requistion stuff

--- public/assets/js/staff_admin/requisition.js
+++ public/assets/js/staff_admin/requisition.js
@@ -1,27 +1,44 @@
-
-/*
-var StaffList = [
+staffRecord = [
     {
-        'slot1' : 'haha yes',
-        'slot2' : 'why tho ajoserjwipai;oewjgt;ieosruyjhs;oriuay;ertgsaertaerterataertaert',
-        '...' : 'why'
+        'Name': 'Rick Roll',
+        'Department': 'No',
+        'Desired salary' : '696969',
+        'profile_picture': 'https://i.ytimg.com/vi/oHg5SJYRHA0/hqdefault.jpg'
     },
     {
-        'slot1' : 's',
-        'slot2' : 't'
-    }
+        'Name': 'Rick Roll 2.0',
+        'Department': 'Despacito HQ',
+        'Desired salary' : '100',
+        'profile_picture': 'https://static.stereogum.com/blogs.dir/2/files/2008/03/rickroll-still-compressed.jpg'
+    },
+    {
+        'Name': 'Jeff',
+        'Department': 'Street',
+        'Desired salary' : '35000',
+        'profile_picture': 'https://i.kym-cdn.com/entries/icons/original/000/016/894/mynameehhjeff.jpg'
+    },
+
 ]
-for(i = 0; i<StaffList.length;i++){
-    var infos = document.getElementsByTagName('StaffList')[i];
-	var info = infos.getElementsByTagName('p')[i];
-	infos.removeChild(info)
-	for (const [key, value] of Object.entries(StaffList[i])) {
-  		info.getElementsByTagName('keyy')[i].textContent = key
-		info.getElementsByTagName('valuee')[i].textContent = value
-		infos.appendChild(info.cloneNode(true))
+var backup = 0
+
+var staffList = document.getElementsByTagName('staffList')[0]
+var staffCard = staffList.getElementsByTagName('staffCard')[0]
+var infos = staffCard.getElementsByTagName('infoStaff')[0]
+var info = infos.getElementsByTagName('p')[0]
+infos.removeChild(info)
+staffList.removeChild(staffCard)
+staffRecord.forEach(function(detail) {
+    var newStaffCard = staffCard.cloneNode(true)
+    var newInfo = info.cloneNode(true)
+    newStaffCard.getElementsByTagName('profileStaff')[0].style.backgroundImage = `url('${detail['profile_picture']}')`
+    delete detail['profile_picture']
+    for (const [key, value] of Object.entries(detail)) {
+        newInfo.getElementsByTagName('yTitle')[0].textContent = key
+        newInfo.getElementsByTagName('yDetail')[0].textContent = value
+        newStaffCard.getElementsByTagName('infoStaff')[0].appendChild(newInfo.cloneNode(true))
     }
-}
-*/
+    staffList.appendChild(newStaffCard)
+})
 
 function reject(){
     var conf = confirm("Are you sure?");
@@ -31,6 +48,23 @@ function reject(){
     else{
     }
 }
+
+function showUD(){
+    if (cU.style.display === "none") {
+        cU.style.display = "inline";
+      } else {
+        cU.style.display = "none";
+        document.getElementById("cU").checked = false;
+      }
+    if (cD.style.display === "none") {
+        cD.style.display = "inline";
+      } else {
+        document.getElementById("cD").checked = false;
+        cD.style.display = "none";
+      }  
+
+    }
+
 
 function confirmCRUD(){
     var cC = document.getElementById("cC").checked; var cR = document.getElementById("cR").checked;
