860c72e43c8365b36a924b82e01ad2e7d1f2fe34
einsxort <einsxort127@gmail.com>
2019-11-13 17:02:15 +0700

    [UPDATE] Completed recruiting page?

--- server/controllers/staffAdmin.js
+++ server/controllers/staffAdmin.js
@@ -2,11 +2,22 @@ const connector = require("../db/db")
 
 exports.getApplicationForm = async function(request, response) {
     try {
-        var result = await connector.query(`SELECT * FROM user WHERE user_id='${request.user.user_id}'`)
+        let staffStatus = await connector.query(`SELECT * FROM staff_admin_info WHERE staffId='${request.user.user_id}'`)
+        let isStaff = 'false'
+        if (staffStatus[0] && staffStatus[0].length) {
+            if (staffStatus[0][0]['status'] == 'active') {
+                isStaff = 'true'
+            }
+        }
+        if (isStaff == 'true') {
+            response.redirect('/staff_admin/management')
+        }
+        let matchedInfo = await connector.query(`SELECT user_id, birth_date, profile_picture, username, CONCAT(firstname, ' ', lastname) AS name, gender, address FROM user WHERE user_id='${request.user.user_id}'`)
         response.render('staff_admin/recruiting', {
             pageTitle: 'TravelAloha - StaffRecruiting',
             user: request.user,
-            data: JSON.stringify(result[0])
+            onPending: (staffStatus[0] && staffStatus[0].length && staffStatus[0][0]['status'] == 'pending')?'true':'false',
+            data: JSON.stringify(matchedInfo[0][0])
         })
     } catch (error) {
         response.send(`
