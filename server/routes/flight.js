4d8189e159f5d1a28f87496034cfbace560da55f
Anawat <anawat.paothong@mail.kmutt.ac.th>
2019-11-13 14:28:19 +0700

    update

--- server/routes/flight.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-exports.getRegister = (req, res) =>
-  res.render("auth/register", {
-    pageTitle: "TravelAloha - Register",
-    user: req.user
-  });
