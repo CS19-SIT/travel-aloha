1d5ec6fdeb7eb1f9951fc9562d1c347ac7287067
Krittiya.on <krittiya.on@mail.kmutt.ac.th>
2019-11-13 16:13:00 +0700

    [update] user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userDashboardController/user");
 
 
-router.get("/test", userController.getDashboard);
+router.get("/", userController.getDashboard);
 router.get("/editProfile", userController.getEditProfile);
-
-
-
-
-
 
 module.exports = router;
