254514b4cabdd47e8c6b5c6ccd658107ca43918a
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2019-11-13 16:34:44 +0700

    [EDIT] userManagement controller

--- server/controllers/userManagement.js
+++ server/controllers/userManagement.js
@@ -1,14 +1,18 @@
 const connector = require("../db/db")
 
 exports.getUsersPage = function(req, res){
-    let query = "SELECT * FROM user"; 
-    connector.query(query,function (err , result){
-        res.render('userManagement/users', {
-            user:result,
-            pageTitle: 'TravelAloha - Usermanagement'
+    let query = "SELECT * FROM user ORDER BY user_id ASC"; 
+    
+    // execute query
+    connector.query(query, (err, result) => {
+        // if (err) {
+        //     res.redirect('/');
+        // }
+        res.render('userManagement/users.ejs', {
+            pageTitle: "Users Management"
+            ,user: result
         });
     });
-    
 }
 
 exports.addUsersPage = function(req,res) {
@@ -19,39 +23,23 @@ exports.addUsersPage = function(req,res) {
 }
 
 exports.editUsersPage = function(req,res) {
-    let user_id = req.params.id;
-    let query = "SELECT * FROM `users` WHERE user_id = '" + user_id + "' ";
-    db.query(query, (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.render('userManagement/edit-users.ejs', {
-            title: "Edit  User"
-            ,users: result[0]
-            ,message: ''
-        });
+    res.render('userManagement/edit-users.ejs', {
+        pageTitle: " Edit a user"
+        ,message: ''
     });
 }
 
 exports.detailUsersPage = function(req,res) {
-    let user_id = req.params.id;
-    let query = "SELECT * FROM `users` WHERE user_id = '" + user_id + "' ";
-    db.query(query, (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.render('userManagement/detail-users.ejs', {
-            title: ""
-            ,users: result[0]
-            ,message: ''
-        });
+    res.render('userManagement/detail-users.ejs', {
+        pageTitle: " Detail of a user"
+        ,message: ''
     });
 }
 
 exports.deleteUsers = function(req,res) {
     let user_id = req.params.id;
-    let getImageQuery = 'SELECT profile_picture from `users` WHERE user_id = "' + user_id + '"';
-    let deleteUserQuery = 'DELETE FROM users WHERE user_id = "' + user_id + '"';
+    let getImageQuery = 'SELECT profile_picture from `user` WHERE user_id = "' + user_id + '"';
+    let deleteUserQuery = 'DELETE FROM user WHERE user_id = "' + user_id + '"';
 
     db.query(getImageQuery, (err, result) => {
         if (err) {
@@ -68,89 +56,8 @@ exports.deleteUsers = function(req,res) {
                 if (err) {
                     return res.status(500).send(err);
                 }
-                res.redirect('/users');
+                res.redirect('/');
             });
         });
     });
 }
-
-exports.addUsers = function(req,res) {
-    if (!req.files) {
-        return res.status(400).send("No files were uploaded.");
-    }
-
-    let message = '';
-    let username = req.body.username;
-    let firstname = req.body.firstname;
-    let lastname = req.body.lastname;
-    let gender = req.body.gender;
-    let birth_date = req.body.birth_date;
-    let phoneNumber = req.body.phoneNumber;
-    let userRole = req.body.userRole;
-    let email = req.body.email;
-
-    let uploadedFile = req.files.profile_picture;
-    let image_name = uploadedFile.name;
-    let fileExtension = uploadedFile.mimetype.split('/')[1];
-    image_name = username + '.' + fileExtension;
-
-    let usernameQuery = "SELECT * FROM `users` WHERE username = '" + username + "'";
-
-    db.query(usernameQuery, (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        if (result.length > 0) {
-            message = 'Username already exists';
-            res.render('userManagement/add-users.ejs', {
-                message,
-                title: " Add a new User"
-            });
-        } else {
-            
-            if (uploadedFile.mimetype === 'image/png' || uploadedFile.mimetype === 'image/jpeg' || uploadedFile.mimetype === 'image/gif') {
-
-                uploadedFile.mv(`server/uploads/${image_name}`, (err ) => {
-                    if (err) {
-                        return res.status(500).send(err);
-                    }
-                    
-                    let query = "INSERT INTO `users` (firstname, lastname, gender, birth_date, phoneNumber, userRole, email, profile_picture, username) VALUES ('" +
-                        firstname + "', '" + lastname + "', '" + gender + "', '" + birth_date + "', '" + phoneNumber + "', '" + userRole + "', '" + email + "', '" + image_name + "', '" + username + "')";
-                    db.query(query, (err, result) => {
-                        if (err) {
-                            return res.status(500).send(err);
-                        }
-                        res.redirect('/users');
-                    });
-                });
-            } else {
-                message = "Invalid File format. Only 'gif', 'jpeg' and 'png' images are allowed.";
-                res.render('userManagement/add-users.ejs', {
-                    message,
-                    title: " Add a new users"
-                });
-            }
-        }
-    });
-}
-
-exports.editUsers = function(req,res) {
-    let user_id = req.params.id;
-    let firstname = req.body.firstname;
-    let lastname = req.body.lastname;
-    let gender = req.body.gender;
-    let phoneNumber = req.body.phoneNumber;
-    let email = req.body.email;
-    let query = "UPDATE `users` SET `firstname` = '" + firstname + "', `lastname` = '" + lastname + "', `gender` = '" + gender + "', `phoneNumber` = '" + phoneNumber + "', `email` = '" + email + "' WHERE `users`.`user_id` = '" + user_id + "'";
-    db.query(query, (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.redirect('/users');
-    });
-}
-
-
-
-
