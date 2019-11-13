064d9bd80bf8ba5e6bda800708eb709c4d29d652
flukerbooker <win.vorapol52@gmail.com>
2019-11-13 16:44:45 +0700

    [UPDATE] Delete comment

--- server/routes/Contact_System.js
+++ server/routes/Contact_System.js
@@ -9,39 +9,4 @@ router.get("/newAirlineInfo", Contact_SystemController.getAirlineInfo);
 router.get("/newHotelInfo", Contact_SystemController.getHotelInfo);
 router.get("/hotelDetail", Contact_SystemController.getHotelDetail);
 
-// router.get('/', function (req, res, next) {
-//   res.render('Contact_System/landingPage', {
-//     pageTitle: 'TravelAloha-ContactSystem',
-//     user: req.user
-//   });
-// });
-
-// router.get('/informationFlow', function (req, res, next) {
-//   res.render('Contact_System/informationFlow', {
-//     pageTitle: 'TravelAloha-Information',
-//     user: req.user
-//   });
-// });
-
-// router.get('/newAirlineInfo', function (req, res, next) {
-//   res.render('Contact_System/newAirlineInfo', {
-//     pageTitle: 'TravelAloha-NewAirline',
-//     user: req.user
-//   });
-// });
-
-// router.get('/newHotelInfo', function (req, res, next) {
-//   res.render('Contact_System/newHotelInfo', {
-//     pageTitle: 'TravelAloha-NewHotel',
-//     user: req.user
-//   });
-// });
-
-// router.get('/hotelDetail', function (req, res, next) {
-//   res.render('Contact_System/hotelDetail', {
-//     pageTitle: 'TravelAloha-Detail',
-//     user: req.user
-//   });
-// });
-
 module.exports = router;
