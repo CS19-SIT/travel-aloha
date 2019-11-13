ded48c6cd3828fd4c6be5ae196f5c4da241cfcde
Sakdipat Ontoum <sakdipat3536@gmail.com>
2019-11-14 00:32:40 +0700

    [EDIT] add router dot post /charge. but no quite perfect.

--- server/routes/checkout.js
+++ server/routes/checkout.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const stripe = require('stripe')('sk_test_c8Sj0KgrzEbhjUJFj7vDC84w00OVqNpUbO');
 
 
 router.get('/', function(req,res) {
@@ -8,5 +9,27 @@ router.get('/', function(req,res) {
         user: req.user
     });
 });
-
+router.post("/charge", (req, res) => {
+    try {
+        stripe.customers
+            .create({
+                name: "req.body.username",
+                // email: req.body.email,
+                source: req.body.stripeToken
+            })
+            .then(customer =>
+                stripe.charges.create({
+                    // amount: req.body.amount * 100,
+                    amount:  100,
+                    currency: "usd",
+                    customer: 1
+                })
+            )
+            .then(() => res.render("index"))
+            .catch(err => console.log(err));
+    } catch (err) {
+        console.log("ERROR",err)
+        res.send(err);
+    }
+});
 module.exports = router;
