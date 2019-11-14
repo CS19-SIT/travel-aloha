dc70e27ef975696cd24bc2d20042813e04803390
flukerbooker <win.vorapol52@gmail.com>
2019-11-14 19:25:36 +0700

    [EDIT] change file name & edit controller

--- server/controllers/Contact_System.js
+++ server/controllers/Contact_System.js
@@ -1,30 +1,32 @@
-exports.getContactSystem = (req, res) =>
+const Contact_SystemModel = require("../models/Contact_System");
+exports.getContactSystem = (req, res) => {
     res.render('Contact_System/landingPage', {
         pageTitle: 'TravelAloha-ContactSystem',
         user: req.user
     });
-
-exports.getInformation = (req, res) =>
+}
+exports.getInformation = (req, res) => {
     res.render('Contact_System/informationFlow', {
         pageTitle: 'TravelAloha-Information',
         user: req.user
     });
+}
 
-
-exports.getAirlineInfo = (req, res) =>
+exports.getAirlineInfo = (req, res) => {
     res.render('Contact_System/newAirlineInfo', {
         pageTitle: 'TravelAloha-NewAirline',
         user: req.user
     });
-
-exports.getHotelInfo = (req, res) =>
+}
+exports.getHotelInfo = (req, res) => {
     res.render('Contact_System/newHotelInfo', {
         pageTitle: 'TravelAloha-NewHotel',
         user: req.user
     });
-
-exports.getHotelDetail = (req, res) =>
+}
+exports.getHotelDetail = (req, res) => { 
     res.render('Contact_System/hotelDetail', {
         pageTitle: 'TravelAloha-Detail',
         user: req.user
     });
+}
