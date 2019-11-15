97f7e5f275accf249bf9437a78507238b6083c72
flukerbooker <win.vorapol52@gmail.com>
2019-11-16 01:15:39 +0700

    [REFACTOR] controller

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -1,99 +1,98 @@
 const contactModel = require("../models/contact");
 
-
 exports.getIndex = (req, res) => {
-    res.render('contact/index', {
-        pageTitle: 'TravelAloha - Contact',
-        user: req.user
-    });
-}
+  res.render("contact/index", {
+    pageTitle: "TravelAloha - Contact",
+    user: req.user
+  });
+};
 exports.getDashboard = (req, res) => {
-    res.render('contact/dashboard', {
-        pageTitle: 'TravelAloha - Contact - Dashboard',
-        user: req.user
-    });
-}
+  res.render("contact/dashboard", {
+    pageTitle: "TravelAloha - Contact - Dashboard",
+    user: req.user
+  });
+};
 
 exports.getAirlineInfo = (req, res) => {
-    res.render('contact/add-new-airline', {
-        pageTitle: 'TravelAloha - Contact - Register New Airline',
-        user: req.user
-    });
-}
+  res.render("contact/add-new-airline", {
+    pageTitle: "TravelAloha - Contact - Register New Airline",
+    user: req.user
+  });
+};
 exports.postAirlineInfo = async (req, res) => {
-    const {
-        airlineName,
-        airlineEmail,
-        airlineAddress,
-        airlineNationality,
-        airlineTelNumber,
-        airlineContactNumber,
-        airlineDescription
-    } = req.body;
+  const {
+    airlineName,
+    airlineEmail,
+    airlineAddress,
+    airlineNationality,
+    airlineTelNumber,
+    airlineContactNumber,
+    airlineDescription
+  } = req.body;
 
-    try {
-        await contactModel.insertNewAirline({
-            airlineName,
-            airlineNationality,
-            airlineEmail,
-            airlineDescription,
-            airlineAddress,
-            airlineTelNumber,
-            airlineContactNumber
-        });
-        res.redirect('dashboard');
-        res.sendStatus(200);
-    } catch (error) {
-        res.sendStatus(400);
-        throw new Error(`[ERR] insertNewHotel: ${error}`);
-    }
-}
+  try {
+    await contactModel.insertNewAirline({
+      airlineName,
+      airlineNationality,
+      airlineEmail,
+      airlineDescription,
+      airlineAddress,
+      airlineTelNumber,
+      airlineContactNumber
+    });
+    res.redirect("dashboard");
+    res.sendStatus(204);
+  } catch (error) {
+    res.sendStatus(404);
+    throw new Error(`[ERR] insertNewHotel: ${error}`);
+  }
+};
 exports.getHotelInfo = (req, res) => {
-    res.render('contact/add-new-hotel', {
-        pageTitle: 'TravelAloha - Contact - Register New Hotel',
-        user: req.user
-    });
-}
+  res.render("contact/add-new-hotel", {
+    pageTitle: "TravelAloha - Contact - Register New Hotel",
+    user: req.user
+  });
+};
 exports.postHotelInfo = async (req, res) => {
-    const {
-        hotelName,
-        hotelEmail,
-        hotelDescription,
-        hotelAddress,
-        hotelTelNumber,
-        hotelContactNumber
-    } = req.body;
+  const {
+    hotelName,
+    hotelEmail,
+    hotelDescription,
+    hotelAddress,
+    hotelTelNumber,
+    hotelContactNumber
+  } = req.body;
 
-    try {
-        await contactModel.insertNewHotel({
-            hotelName,
-            hotelDescription,
-            hotelAddress,
-            hotelTelNumber,
-            hotelContactNumber,
-            hotelEmail
-        });
-        res.redirect('dashboard');
-        res.sendStatus(200);
-    } catch (error) {
-        res.sendStatus(400);
-        throw new Error(`[ERR] insertNewHotel: ${error}`);
-    }
-}
-exports.getHotelDetail = (req, res) => {
-    try{
-        res.render('contact/new-hotel-detail', {
-            pageTitle: 'TravelAloha - Contact - New Hotel Detail',
-            user: req.user
-        });
-        res.sendStatus(204);
-    }catch(error){
-        res.sendStatus(404);
-    }
-}
-exports.getAirlineDetail = (req, res) => {
-    res.render('contact/new-airline-detail', {
-        pageTitle: 'TravelAloha - Contact - New Airline Detail',
-        user: req.user
+  try {
+    await contactModel.insertNewHotel({
+      hotelName,
+      hotelDescription,
+      hotelAddress,
+      hotelTelNumber,
+      hotelContactNumber,
+      hotelEmail
     });
-}
+    res.redirect("dashboard");
+    res.sendStatus(204);
+  } catch (error) {
+    res.sendStatus(404);
+    throw new Error(`[ERR] insertNewHotel: ${error}`);
+  }
+};
+exports.getHotelDetail = (req, res) => {
+  try {
+    res.render("contact/new-hotel-detail", {
+      pageTitle: "TravelAloha - Contact - New Hotel Detail",
+      user: req.user
+    });
+    res.sendStatus(204);
+  } catch (error) {
+    res.sendStatus(404);
+  }
+};
+exports.getAirlineDetail = (req, res) => {
+  res.render("contact/new-airline-detail", {
+    pageTitle: "TravelAloha - Contact - New Airline Detail",
+    user: req.user
+  });
+};
