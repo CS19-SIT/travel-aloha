15a7531b3216f3f3f764e7a428233207080b6ece
flukerbooker <win.vorapol52@gmail.com>
2019-11-16 23:16:53 +0700

    [FIXED] frond end

--- server/routes/contact/index.js
+++ server/routes/contact/index.js
@@ -2,14 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 const contactController = require("../../controllers/contact/index");
-//const authMiddleware = require("../../middlewares/auth"); wait for it
+const authMiddleware = require("../../middlewares/auth");
 
 router.get("/", contactController.getIndex);
-router.get("/dashboard", contactController.getDashboard);
-router.get("/add-new-airline", contactController.getAirlineInfo);
-router.post("/add-new-airline", contactController.postAirlineInfo);
 router.get("/add-new-hotel", contactController.getHotelInfo);
 router.post("/add-new-hotel", contactController.postHotelInfo);
-router.get("/dashboard/detail", contactController.getHotelDetail);
+router.get("/add-new-airline", contactController.getAirlineInfo);
+router.post("/add-new-airline", contactController.postAirlineInfo);
+router.get("/dashboard", 
+    contactController.getDashboard, 
+    authMiddleware.isAuthenticated,
+    authMiddleware.isStaff
+);
+router.get("/dashboard/detail", 
+contactController.getHotelDetail,
+authMiddleware.isAuthenticated,
+authMiddleware.isStaff
+);
 
 module.exports = router;
