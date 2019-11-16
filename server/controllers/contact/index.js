c70c9b2a140a4f2cc10ab41f5c86a90b843ef58f
flukerbooker <win.vorapol52@gmail.com>
2019-11-16 22:38:17 +0700

    [EDIT] Final before merge to dev

--- server/controllers/contact/index.js
+++ server/controllers/contact/index.js
@@ -1,5 +1,16 @@
 const contactModel = require("../../models/contact");
-
+exports.getIndex = (req, res) => {
+  res.render("contact/index", {
+    pageTitle: "TravelAloha - Contact",
+    user: req.user
+  });
+};
+exports.getDashboard = (req, res) => {
+  res.render("contact/dashboard", {
+    pageTitle: "TravelAloha - Contact - Dashboard",
+    user: req.user
+  });
+};
 exports.getHotelInfo = (req, res) => {
   res.render("contact/add-new-hotel", {
     pageTitle: "TravelAloha - Contact - Register New Hotel",
