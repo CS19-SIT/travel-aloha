bacd3d885c2a29be59bde9a7c19ae1a3630fde7a
Jasonnonstop10 <jay44411@gmail.com>
2019-11-19 02:42:26 +0700

    [add] star algorithm and sorting algorithm but non-use-it

--- public/assets/js/star.js
+++ public/assets/js/star.js
@@ -1,1 +1,25 @@
 //star function
+function displayStar(dots,score){
+    const allStar=Math.floor(score);
+    const halfstar=(allStar<score);
+    const tStar=Math.ceil(5-score);
+    for(var i=1;i<=score;i++){
+      $('div.dot-'+i,dots).addClass('green-dot');
+    }
+    if(halfstar){
+      $('.dot-'+i,dots).addClass('yellow-dot');
+    }
+  }
+
+function displaytextscore(score){
+    if(score<2){
+        $('#text').addClass('red-text');
+    }
+    else if(score>=2&&score<3.5){
+        $('#text').addClass('yellow-text');
+    }
+    else{
+        $('#text').addClass('green-text');
+    }
+
+}
