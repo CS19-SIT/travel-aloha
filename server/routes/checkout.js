cc2f6c32c301f6ec6d60102f6f95b132f4b9cfb9
Sakdipat Ontoum <sakdipat3536@gmail.com>
2019-11-21 00:20:05 +0700

    [EDIT] make router on get and post connect to checkoutController.

--- server/routes/checkout.js
+++ server/routes/checkout.js
@@ -1,38 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')('sk_test_c8Sj0KgrzEbhjUJFj7vDC84w00OVqNpUbO');
 
+const checkoutController = require("../../server/controllers/paymentController/checkout");
+// const authMiddleware = require("../../middlewares/auth");
 
-router.get('/', function(req,res) {
-    res.render('payment/checkout',{
-        pageTitle: 'TravelAloha-Checkout',
-        user: req.user
-    });
-});
-router.post("/charge", (req, res) => {
-    try {
-        stripe.customers
-            .create({
-                name: "req.body.username",
-                // email: req.body.email,
-                source: req.body.stripeToken
-            })
-            .then(customer =>
-                stripe.charges.create({
-                    // amount: req.body.amount * 100,
-                    amount:  100,
-                    currency: "usd",
-                    customer: customer.id
-                })
-            )
-            .then(() => res.render('payment/completedPayment',{
-                pageTitle: 'TravelAloha-completed',
-                user: req.user
-            }))
-            .catch(err => console.log(err));
-    } catch (err) {
-        console.log("ERROR",err)
-        res.send(err);
-    }
-});
+router.get("/",checkoutController.getIndex);
+
+router.post("/charge", checkoutController.postIndex);
+
 module.exports = router;
