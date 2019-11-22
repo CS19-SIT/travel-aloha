a4e98d796f0abd8b02420b70e8c53ae800d45082
basputtipong <basputtipongz@gmail.com>
2019-11-22 15:04:52 +0700

    fix bug

--- server/controllers/user/dashboard/favorite.js
+++ server/controllers/user/dashboard/favorite.js
@@ -1,5 +1,17 @@
-exports.getIndex = (req, res) =>
-  res.render("fav/favorite", {
-    pageTitle: "TravelAloha - Dashboard - Favorite",
-    user: req.user
-  });
+const connector = require("../../../db/db")
+// exports.getIndex = (req, res) =>
+//   res.render("fav/favorite", {
+//     pageTitle: "TravelAloha - Dashboard - Favorite",
+//     user: req.user
+//   });
+
+
+  exports.getIndex = function(req,res){
+      let query = "SELECT * FROM fav_hotel as f,hotel as h WHERE f.favHotelID = h.hotelID";
+      connector.query(query,function(err,result){
+          res.render('fav/favorite',{
+              fav:result, 
+              pageTitle: 'TravelAloha'
+          });
+      });
+  }
