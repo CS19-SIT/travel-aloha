609926c0ea0552fc501a5a950998b2811db6c34f
Sakdipat Ontoum <sakdipat3536@gmail.com>
2019-11-23 22:12:07 +0700

    [EDIT] wirte code to control the data from database for show in front-end

--- server/controllers/paymentController/checkout.js
+++ server/controllers/paymentController/checkout.js
@@ -1,30 +1,44 @@
 const stripe = require('stripe')('sk_test_c8Sj0KgrzEbhjUJFj7vDC84w00OVqNpUbO');
 
-exports.getIndex = (req, res) => {
-    res.render("payment/checkout", {
+//hotel
+const hotelBooking = require('../../models/checkoutModels/chekout'); 
+
+
+exports.getIndex = async (req, res) => {
+    try{
+        let data = await hotelBooking.retrieveBookingDetail();
+        let data2 = await hotelBooking.retrieveBookingHead();
+        res.render("payment/checkout", {
         pageTitle: "TravelAloha - Checkout",
-        user: req.user
+        user: req.user,
+        booking_detail: data,
+        booking_head: data2
     });
+} catch (err) {
+    res.sendStatus(404);
+}
 };
-exports.postIndex = (req, res) => {
+
+exports.postIndex = async (req, res) => {
     try {
         stripe.customers
             .create({
-                name: "req.body.username",
-                // email: req.body.email,
+                name: req.body.username,
+                email: req.body.email,
                 source: req.body.stripeToken
             })
             .then(customer =>
                 stripe.charges.create({
-                    // amount: req.body.amount * 100,
-                    amount: 100,
-                    currency: "usd",
+                    amount: req.body.amount * 100,
+                    amount: 10000,
+                    currency: "thb",
                     customer: customer.id
                 })
             )
             .then(() => res.render("payment/completedPayment", {
                 pageTitle: 'TravelAloha-completed',
                 user: req.user
+                
             }))
             .catch(err => console.log(err));
     } catch (err) {
