9710e765538ed7e73b9647234868003c8e47e6f6
Jasonnonstop10 <jay44411@gmail.com>
2019-12-01 12:50:40 +0700

    [Test]ajax but fail

--- public/assets/js/review.js
+++ public/assets/js/review.js
@@ -1,5 +1,16 @@
+$(document).ready(function(){
 $('.like, .dislike').on('click', function() {
-  event.preventDefault();
-  $('.active').removeClass('active');
-  $(this).addClass('active');
+    event.preventDefault();
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+});
+$('#hotelsend').click(function(){
+  $.get("/review/hotel",{
+    hotel_hotelId:"55"
+  },
+  function(data,status){
+    alert("Data: " + data + "\nStatus: " + status);
+  });
+});
+
 });
