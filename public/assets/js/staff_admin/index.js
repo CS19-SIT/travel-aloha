70e6535f97511c3998145c9b7eadeed53c9e95bb
einsxort <einsxort127@gmail.com>
2019-12-11 00:19:55 +0700

    [ADD] Detail reload

--- public/assets/js/staff_admin/index.js
+++ public/assets/js/staff_admin/index.js
@@ -6,3 +6,19 @@ setTimeout(() => {
         windowHeight = document.body.clientHeight;
     }
 }, 1000);
+
+function reloadRolesList(elem) {
+    $.ajax({
+        url: '/admin/staff/getQuery', method: 'POST',
+        data: {
+            sql: `SELECT  * FROM staff_role WHERE deptNo='${elem.value}'`
+        }
+    }).done((result) => {
+        if (result.status == 200) {
+            if (!result.data.length) {
+                result.data = 'No available role'
+            }
+            document.getElementById('rolesDetail').textContent = JSON.stringify(result.data);
+        }
+    });
+}
