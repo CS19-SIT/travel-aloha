df6c7dc11e4ed061712a8221236cc42c129bbc0a
einsxort <einsxort127@gmail.com>
2019-12-11 16:24:18 +0700

    [ADD] PhotogridDepartment for index

--- public/assets/js/staff_admin/index.js
+++ public/assets/js/staff_admin/index.js
@@ -7,11 +7,19 @@ setTimeout(() => {
     }
 }, 1000);
 
+function lookupDetails(index) {
+    const prevSelectedDept = document.getElementById('deptListSelect').selectedIndex;
+    if (prevSelectedDept != parseInt(index, 10)) {
+        document.getElementById('deptListSelect').selectedIndex = parseInt(index, 10);
+        document.getElementById('deptListSelect').dispatchEvent(new Event('change'));
+    }
+}
+
 function reloadRolesList(elem) {
     $.ajax({
         url: '/admin/staff/getQuery', method: 'POST',
         data: {
-            sql: `SELECT  * FROM staff_role WHERE deptNo='${elem.value}'`
+            sql: `SELECT roleName, roleDetail FROM staff_role WHERE deptNo='${elem.value}'`
         }
     }).done((result) => {
         if (result.status == 200) {
@@ -19,7 +27,7 @@ function reloadRolesList(elem) {
                 document.getElementById('rolesDetail').innerHTML = 'No available role';
             } else {
                 document.getElementById('rolesDetail').innerHTML = result.data.map((item, index) => {
-                    return `<p class="font-weight-bold">${index+1}) ${item.roleName}</p><p>${item.roleDetail}</p>`;
+                    return `<p class="font-weight-bold">${index+1}) ${item.roleName}</p><p class="text-break">${item.roleDetail}</p>`;
                 }).join('');
             }
         }
