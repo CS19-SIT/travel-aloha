94c7396c7087ad75d6a51a2ce29b5c8066831a9e
milk <patthamaporn.milk@mail.kmutt.ac.th>
2019-12-12 17:50:59 +0700

    [Update] QUERY COMPLETE
    just get one result not complete yet.
    ได้แล้วโว้ยยยยยย

--- server/controllers/query_reward_con.js
+++ server/controllers/query_reward_con.js
@@ -1,9 +1,10 @@
 const qc = require("../models/query_coupon");
 
 
-exports.getCoupon = (req, res ) => {
+exports.getCoupon = async (req, res ) => {
   // res.send("Im from controller query coupon");
-  const result = qc.showValidCouponAC("AC1234");
-      console.log(result);
+    let result = await qc.showValidCouponAC("AC1234");
+    console.log(result);
+
 
 };
--- server/models/query_coupon.js
+++ server/models/query_coupon.js
@@ -1,20 +1,36 @@
 const db = require("../db/db");
 
-exports.showValidCouponAC = async AccCouID => {
-  const accoupon =  db.query("SELECT * FROM AccommodationCoupon WHERE AccCouID = ?",
- [AccCouID]);
- //return stm
-};
+exports.showValidCouponAC = async (AccCouID) => {
+  const accoupon =  await db.execute("SELECT * FROM `AccommodationCoupon` WHERE `AccCouID` = ?",
+  [AccCouID]);
+  return accoupon;
+  };
 
-exports.showValidCouponFl = async LevelID => {
-  const flcoupon =  db.query("SELECT CouponCode FROM ValidCoupon as v, FlightCoupon_copy as f WHERE v.couponID = f.FlCouID AND user_ID = ?",
- [LevelID]);
-};
-exports.showValidCouponEn = async LevelID => {
-  const encoupon =  db.query("SELECT CouponCode FROM ValidCoupon as v , EntertainmentCoupon as e WHERE v.couponID = e.EntCouID AND user_ID = ?",
- [LevelID]);
-};
-exports.showValidCouponFo = async LevelID => {
-  const focoupon =  db.query("SELECT CouponCode FROM ValidCoupon as v, FoodCoupon as fo WHERE v.couponID = fo.FoCouID AND user_ID = ?",
- [LevelID]);
-};
+
+
+  // let AuthUser = function(data) {
+  //   return google.login(data.username, data.password).then(token => { return token } )
+  // }
+// with placeholder
+// connection.query(
+//   'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//   ['Page', 45],
+//   function(err, results) {
+//     console.log(results);
+//   }
+// );
+ //return stm
+
+
+// exports.showValidCouponFl = async LevelID => {
+//   const flcoupon =  db.query("SELECT CouponCode FROM ValidCoupon as v, FlightCoupon_copy as f WHERE v.couponID = f.FlCouID AND user_ID = ?",
+//  [LevelID]);
+// };
+// exports.showValidCouponEn = async LevelID => {
+//   const encoupon =  db.query("SELECT CouponCode FROM ValidCoupon as v , EntertainmentCoupon as e WHERE v.couponID = e.EntCouID AND user_ID = ?",
+//  [LevelID]);
+// };
+// exports.showValidCouponFo = async LevelID => {
+//   const focoupon =  db.query("SELECT CouponCode FROM ValidCoupon as v, FoodCoupon as fo WHERE v.couponID = fo.FoCouID AND user_ID = ?",
+//  [LevelID]);
+// };
