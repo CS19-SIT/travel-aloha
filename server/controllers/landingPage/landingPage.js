49e5e0d56b41b2b237b8d09c13da35460bacc13e
WARAKORN CHANTRANUPONG <boydoubleb@gmail.com>
2019-12-14 16:43:33 +0700

    finish

--- server/controllers/landingPage/landingPage.js
+++ server/controllers/landingPage/landingPage.js
@@ -1,6 +1,26 @@
-exports.getIndex = (req, res) => {
-    res.render("landingPage/userLanding", {
-      pageTitle: "TravelAloha - Dashboard - History",
-      user: req.user
-    });
-  };
+const page = require("../../models/landingPage");
+const hotel = require("../../models/admin-hotel");
+exports.getIndex = async (req, res) => {
+try{
+  let data = await page.countUsers();  
+  let dataHotel = await page.countHotel();
+  let dataPlace = await page.countPlace();
+  let hotelInfo = await hotel.getAllHotel();
+  let flightInfo = await page.getAllFlight();
+
+  res.render("landingPage/userLanding", {
+    pageTitle: "TravelAloha - Dashboard - History",
+    user: req.user,
+    data: data,
+    dataHotel : dataHotel,
+    dataPlace : dataPlace,
+    hotelInfo : hotelInfo,
+    flightInfo : flightInfo
+ 
+    
+  });
+}catch(err){
+  res.sendStatus(404);
+}
+    
+};
