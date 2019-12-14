5506da17d3f48ba6ff1781336631f2e66a6a149b
einsxort <einsxort127@gmail.com>
2019-12-14 14:06:01 +0700

    [ADD] Read update

--- public/assets/js/staff_admin/homepage.js
+++ public/assets/js/staff_admin/homepage.js
@@ -6,6 +6,11 @@ app.controller('homeController', [
     function($scope, $http) {
         const self = this;
         
+        $scope.viewedStaff = null;
+        $scope.devModeOn = false;
+        self.roleList = [];
+        [$scope.newDept, $scope.newRole] = [false, false];
+
         self.getQuery = (data) => $http({
             url: '/admin/staff/getQuery',
             method: 'POST',
@@ -16,5 +21,96 @@ app.controller('homeController', [
             method: 'POST',
             data: data
         }).then((res) => res.data);
+
+        self.openDevMode = () => {
+            $scope.devModeOn = true;
+        };
+        self.updateViewedStaff = (staff) => {
+            $scope.viewedStaff = null;
+            setTimeout(() => {
+                $scope.viewedStaff = staff;
+                self.updateDept({deptNo: staff.deptNo, deptName: staff.deptName});
+            }, 100);
+        };
+        self.updateInfo = (staff, dept, role, salary) => {
+            try {
+                if (dept && role && salary > 0) {
+                    if (staff.isManager == 'true') {
+                        self.sendQuery({
+                            sql: `UPDATE staff_info SET salary=${salary} WHERE staffId='${staff.user_id}'`
+                        }).then((result) => {
+                            if (result.status == 200) {
+                                self.setNewStaffList();
+                            } else {
+                                throw new Error(result.message);
+                            }
+                        });
+                    } else {
+                        self.getQuery({
+                            sql: `SELECT 1 FROM staff_manager WHERE staffId='${staff.user_id}'`
+                        }).then((result) => {
+                            if (result.status == 200 && !result.data.length) {
+                                self.sendQuery({
+                                    sql: `UPDATE staff_info SET deptNo='${dept.deptNo}', roleId='${role.roleId}', salary=${salary} WHERE staffId='${staff.user_id}'`
+                                }).then((result) => {
+                                    if (result.status == 200) {
+                                        self.setNewStaffList();
+                                    } else {
+                                        throw new Error(result.message);
+                                    }
+                                });
+                            } else {
+                                alert('you can\'t change department of managers');
+                                self.setNewStaffList();
+                            }
+                        });
+                    }
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        self.cancelView = () => {
+            $scope.viewedStaff = null;
+        };
+        self.closeDevMode = () => {
+            $scope.devModeOn = false;
+            self.cancelView();
+        };
+        self.updateDept = (dept) => {
+            $scope.newDept = dept;
+            $scope.newRole = false;
+            self.getQuery({
+                sql: `SELECT roleId, roleName FROM staff_role WHERE deptNo='${dept.deptNo}'`
+            }).then((result) => {
+                if (result.status == 200) {
+                    self.roleList = result.data;
+                    $scope.newRole = self.roleList[0];
+                }
+            });
+        };
+        self.updateRole = (role) => {
+            $scope.newRole = role;
+        }
+
+        self.setNewStaffList = () => {
+            self.getQuery({
+                sql: `SELECT user_id, IF(profile_picture IS NULL, '', profile_picture) AS profile_picture, CONCAT(firstname, ' ', lastname) AS name, sr.deptNo, deptName, sr.roleId, roleName, salary, latestCheckIn,
+                    CASE WHEN EXISTS(SELECT 1 FROM staff_manager sm WHERE sm.staffId=si.staffId)
+                        THEN 'true'
+                        ELSE 'false'
+                    END AS isManager  
+                    FROM user u, staff_info si, staff_department sd, staff_role sr 
+                    WHERE	si.status='active'
+                        AND si.staffId=u.user_id
+                        AND si.deptNo=sd.deptNo 
+                        AND sd.deptNo=sr.deptNo
+                        AND si.roleId=sr.roleId
+                        ORDER BY isManager DESC, latestCheckIn`
+            }).then((result) => {
+                $scope.staffs = result.data;
+                self.cancelView();
+            })
+        };
     }
 ]);
