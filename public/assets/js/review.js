dec788f2ecb5abe73d1a87df73d44aae105f44e5
Krerkthad <kerkthud_1@hotmail.com>
2019-12-14 18:04:15 +0700

    [UPDATE] hotel

--- public/assets/js/review.js
+++ public/assets/js/review.js
@@ -1,24 +1,8 @@
-$(document).ready(function(){
-$('.like, .dislike').on('click', function() {
+$(document).ready(function() {
+  $(".like, .dislike").on("click", function() {
     event.preventDefault();
-    $('.active').removeClass('active');
-    $(this).addClass('active');
-});
-$('.fa-question-circle').tooltip('enable');
-$("#hotelsend").submit(function(event) {
-  event.preventDefault();
-  $.ajax({
-    url:"/review/hotel",
-    method: "POST",
-    data: { hotel_hotelId : "2"},
-    type:json,
-    success: function(data) {
-      alert("Data updated!");
-      location.reload();
-    },
-    error: function(data) {
-      console.log("ERR: " + data);
-    }
+    $(".active").removeClass("active");
+    $(this).addClass("active");
   });
-});
+  $(".fa-question-circle").tooltip("enable");
 });
