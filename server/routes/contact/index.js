0d9435a8487592956461210c130d768edc7fc77d
flukerbooker <win.vorapol52@gmail.com>
2019-12-15 00:52:05 +0700

    [RESOLVED] fix bug

--- server/routes/contact/index.js
+++ server/routes/contact/index.js
@@ -5,28 +5,16 @@ const contactController = require("../../controllers/contact/index");
 const authMiddleware = require("../../middlewares/auth");
 
 router.get("/", contactController.getIndex);
-
-router.get("/add-new-airline", function(req, res, next) {
-  res.render("contact/add-new-airline", {
-    pageTitle: "TravelAloha - Contact - Register New Airline",
-    user: req.user
-  });
-});
-
-router.get("/add-new-hotel", function(req, res, next) {
-  res.render("contact/add-new-hotel", {
-    pageTitle: "TravelAloha - Contact - Register New Hotel",
-    user: req.user
-  });
-});
-
+router.get("/add-new-hotel", contactController.getHotelInfo);
+router.post("/add-new-hotel", contactController.postHotelInfo);
+router.get("/add-new-airline", contactController.getAirlineInfo);
+router.post("/add-new-airline", contactController.postAirlineInfo);
 router.get(
   "/new-hotel-dashboard",
   authMiddleware.isAuthenticated,
   authMiddleware.isStaff,
   contactController.getHotelDashboard
 );
-
 router.get(
   "/new-airline-dashboard",
   authMiddleware.isAuthenticated,
