bbf3f3e8dfb4da7f964aefdd19314a110c15f8ec
Krerkthad <kerkthud_1@hotmail.com>
2019-12-16 15:23:57 +0700

    [UPDATE] Airline

--- server/controllers/review/index.js
+++ server/controllers/review/index.js
@@ -8,8 +8,8 @@ exports.getHotel = async (req, res) => {
     const { Type_Of_Hotel_Reviewer, Sort, Score} = req.query;
     const hotelReview = await Rating_ReviewModel.getHotelReviewInfo(id, Type_Of_Hotel_Reviewer, Sort, Score);
 
-    console.log(Type_Of_Hotel_Reviewer, Sort, Score);
-    console.log(req.query);
+    // console.log(Type_Of_Hotel_Reviewer, Sort, Score);
+    // console.log(req.query);
     if (hotelReview[0].length == 0) {
       return res.status(404).render("errors/404", {
         pageTitle: "TravelAloha - Page Not Found",
@@ -43,7 +43,8 @@ exports.getHotel = async (req, res) => {
 exports.getAirline = async (req, res) => {
     try {
       const { id } = req.params;
-      const airlineReview = await Rating_ReviewModel.getAirlineReviewInfo(id);
+      const { Type_Of_Airline_Reviewer, Sort, Score} = req.query;
+      const airlineReview = await Rating_ReviewModel.getAirlineReviewInfo(id, Type_Of_Airline_Reviewer, Sort, Score);
       if (airlineReview[0].length == 0) {
         return res.status(404).render("errors/404", {
           pageTitle: "TravelAloha - Page Not Found",
@@ -59,6 +60,7 @@ exports.getAirline = async (req, res) => {
         moment
       });
     } catch (getAirlineError) {
+          // console.log(getHotelError);
       res.status(500).render("errors/500", {
         pageTitle: "TravelAloha - Bad Request",
         user: req.user,
