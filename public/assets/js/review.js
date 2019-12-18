7a5b9fc5b331b522c53a2a814f349bfa3f98c39a
Jasonnonstop10 <jay44411@gmail.com>
2019-12-18 22:53:52 +0700

    [fix] wrire review

--- public/assets/js/review.js
+++ public/assets/js/review.js
@@ -1,7 +1,6 @@
 function summit(){
     $('#exampleModal').modal('hide');
-    window.location.reload(true);
-    document.getElementById("inputset").reset(); 
+    window.location.reload(true); 
 }
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
