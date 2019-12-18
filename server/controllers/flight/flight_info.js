b9089b982d5e751f148519bb8c049651ac182dc7
Anawat Paothong <61130500231@st.sit.kmutt.ac.th>
2019-12-18 21:33:24 +0700

    update

--- server/controllers/flight/flight_info.js
+++ server/controllers/flight/flight_info.js
@@ -25,9 +25,7 @@ exports.getSearchFlight = async (req, res) => {
         }
         const result = await db.query("select f.Departure,f.Destination,a.airlineName,air.Airport_name,df.Depart_date,df.Depart_Time,df.Arrive_Date,df.Arrive_Time,se.Class,se.Price from Flight as f, airline as a, Airport as air, Daily_Flight as df, Seat_Price as se where f.Airline_ID = a.airline_Id and air.Airport_ID = f.destination and  f.Destination like '%"+destination+"' and f.Departure like '%"+origin+"' and se.Class like '%"+seat_class+"' and df.Depart_Date like '%"+check_in+"' order by se.Price asc;");
 
-       if(result != null){
-         console.log(result[0]);
-       }
+  
         // console.log(result[0]);
     res.render("flight/findFlight", {
       pageTitle: "Search Flight " + req.body.destination,
