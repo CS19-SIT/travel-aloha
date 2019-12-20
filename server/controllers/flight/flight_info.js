e0dd6c87ce6d34396e2cdcd63eca162bc0f71170
AnawatPaotthong <47717997+AnawatPaotthong@users.noreply.github.com>
2019-12-20 16:54:04 +0700

    Update flight_info.js

--- server/controllers/flight/flight_info.js
+++ server/controllers/flight/flight_info.js
@@ -23,7 +23,7 @@ exports.getSearchFlight = async (req, res) => {
    if (!origin | !destination | !check_in ){
             throw new Error();
         }
-        const result = await db.query("select f.Departure,f.Destination,a.airlineName,air.Airport_name,df.Depart_date,df.Depart_Time,df.Arrive_Date,df.Arrive_Time,se.Class,se.Price from Flight as f, airline as a, Airport as air, Daily_Flight as df, Seat_Price as se where f.Airline_ID = a.airline_Id and air.Airport_ID = f.destination and  f.Destination like '%"+destination+"' and f.Departure like '%"+origin+"' and se.Class like '%"+seat_class+"' and df.Depart_Date like '%"+check_in+"' and se.Price < "+ price +" and a.airlineName = '" + airline + "' and air.Airport_name = '" + airport + "' order by se.Price asc;");
+        const result = await db.query("select f.Departure,f.Destination,a.airlineName,air.Airport_name,df.Depart_date,df.Depart_Time,df.Arrive_Date,df.Arrive_Time,se.Class,se.Price from Flight as f, airline as a, Airport as air, Daily_Flight as df, Seat_Price as se where f.Airline_ID = a.airline_Id and air.Airport_ID = f.destination and  f.Destination like '%"+destination+"' and f.Departure like '%"+origin+"' and se.Class like '%"+seat_class+"' and df.Depart_Date like '%"+check_in+"' and se.Price < "+ price +" and a.airlineName = '" + airline + "' order by se.Price asc;");
 
   
         // console.log(result[0]);
